931aeb3c174d288a77de0ec319c2c748aa2a6b8e
Iulian Rotaru <iulian@gnotis.ro>
2019-03-25 22:43:52 +0200

    Remove target files.

--- fixture/scripts/build/site/script/js.format.RichText.js
+++
@@ -1,5 +0,0 @@
-$package("js.format");
-
-js.format.RichText = function() {
-	this.description = new js.widget.Description();
-}
--- fixture/strings/build/site/en/script/js.wood.Compo.js
+++
@@ -1,8 +0,0 @@
-$package("js.wood");
-
-js.wood.Compo = function (ownerDoc, node) {
-    this.$super(ownerDoc, node);
-    this.setCaption('Hello');
-    this.setName('Component Name');
-    this.setDescription('This is <strong>script</strong> <em>description</em>.');
-};
