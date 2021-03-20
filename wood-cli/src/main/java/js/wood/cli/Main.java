bee746cfbfb3a3d8a3d6d6b13ed98f6c8df0fa65
Iulian Rotaru <iulian@gnotis.ro>
2021-03-20 05:34:34 +0200

    CLI main checks sub-commands for proper annotation.

--- wood-cli/src/main/java/js/wood/cli/Main.java
+++ wood-cli/src/main/java/js/wood/cli/Main.java
@@ -1,5 +1,8 @@
 package js.wood.cli;
 
+import java.lang.annotation.Annotation;
+
+import js.lang.BugError;
 import js.wood.cli.compo.CompoCommands;
 import js.wood.cli.compo.CompoCreate;
 import js.wood.cli.compo.CompoDelete;
@@ -27,34 +30,47 @@ import picocli.CommandLine.Command;
 public class Main {
 	public static void main(String... args) {
 		CommandLine projectCommands = new CommandLine(ProjectCommands.class);
-		projectCommands.addSubcommand("create", ProjectCreate.class);
-		projectCommands.addSubcommand("build", ProjectBuild.class);
-		projectCommands.addSubcommand("clean", ProjectClean.class);
-		projectCommands.addSubcommand("deploy", ProjectDeploy.class);
-		projectCommands.addSubcommand("destroy", ProjectDestroy.class);
+		projectCommands.addSubcommand(task(ProjectCreate.class));
+		projectCommands.addSubcommand(task(ProjectBuild.class));
+		projectCommands.addSubcommand(task(ProjectClean.class));
+		projectCommands.addSubcommand(task(ProjectDeploy.class));
+		projectCommands.addSubcommand(task(ProjectDestroy.class));
 
 		CommandLine compoCommands = new CommandLine(new CompoCommands());
-		compoCommands.addSubcommand("create", CompoCreate.class);
-		compoCommands.addSubcommand("delete", CompoDelete.class);
-		compoCommands.addSubcommand("rename", CompoRename.class);
-		compoCommands.addSubcommand("move", CompoMove.class);
-		compoCommands.addSubcommand("open", CompoOpen.class);
-		compoCommands.addSubcommand("preview", CompoPreview.class);
-		compoCommands.addSubcommand("import", CompoImport.class);
-		compoCommands.addSubcommand("export", CompoExport.class);
+		compoCommands.addSubcommand(task(CompoCreate.class));
+		compoCommands.addSubcommand(task(CompoDelete.class));
+		compoCommands.addSubcommand(task(CompoRename.class));
+		compoCommands.addSubcommand(task(CompoMove.class));
+		compoCommands.addSubcommand(task(CompoOpen.class));
+		compoCommands.addSubcommand(task(CompoPreview.class));
+		compoCommands.addSubcommand(task(CompoImport.class));
+		compoCommands.addSubcommand(task(CompoExport.class));
 
 		CommandLine runtimeCommands = new CommandLine(new RuntimeCommands());
-		runtimeCommands.addSubcommand("create", RuntimeCreate.class);
-		runtimeCommands.addSubcommand("start", RuntimeStart.class);
-		runtimeCommands.addSubcommand("stop", RuntimeStop.class);
-		runtimeCommands.addSubcommand("destroy", RuntimeDestroy.class);
+		runtimeCommands.addSubcommand(task(RuntimeCreate.class));
+		runtimeCommands.addSubcommand(task(RuntimeStart.class));
+		runtimeCommands.addSubcommand(task(RuntimeStop.class));
+		runtimeCommands.addSubcommand(task(RuntimeDestroy.class));
 
 		CommandLine commandLine = new CommandLine(new Main());
-		commandLine.addSubcommand("project", projectCommands);
-		commandLine.addSubcommand("compo", compoCommands);
-		commandLine.addSubcommand("runtime", runtimeCommands);
-		commandLine.addSubcommand("build", ProjectBuild.class);
+		commandLine.addSubcommand(projectCommands);
+		commandLine.addSubcommand(compoCommands);
+		commandLine.addSubcommand(runtimeCommands);
+		commandLine.addSubcommand(task(ProjectBuild.class));
 
 		System.exit(commandLine.execute(args));
+	}
+
+	private static Object task(Class<? extends Task> taskClass) {
+		Annotation commandAnnotation = taskClass.getAnnotation(Command.class);
+		if (commandAnnotation == null) {
+			throw new BugError("Not annotated task class |%s|.", taskClass);
+		}
+		try {
+			Task task = taskClass.newInstance();
+			return task;
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new BugError("Not instantiable task class |%s|.", taskClass);
+		}
 	}
 }
