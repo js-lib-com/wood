7ebc09ea396523cb28cf974d4bb1e4dc1e504115
Iulian Rotaru <iulian@gnotis.ro>
2021-03-21 12:38:09 +0200

    Java property load method discard backslash before not escaping characters.

--- wood-core/src/test/java/js/wood/api/JavaTest.java
+++ wood-core/src/test/java/js/wood/api/JavaTest.java
@@ -8,7 +8,10 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.io.Writer;
+import java.util.Properties;
 
 import org.junit.Test;
 
@@ -38,14 +41,27 @@ public class JavaTest {
 	public void removeNotWrittenFile() throws IOException {
 		File file = new File("src/test/resources/file");
 		Writer writer = new FileWriter(file);
-		
+
 		// file writer constructor creates the file
 		assertTrue(file.exists());
 		// delete fails because file is still opened by writer
 		assertFalse(file.delete());
-		
+
 		writer.close();
 		assertTrue(file.delete());
 		assertFalse(file.exists());
+	}
+
+	/**
+	 * Property load method discard backslash before not escaping characters. In this example \\ backslashes are loaded as a
+	 * single one and '\r' from \repository begin is loaded as new line.
+	 */
+	@Test
+	public void propertiesLoadBackSlash() throws IOException {
+		Properties properties = new Properties();
+		try (Reader reader = new StringReader("repository.dir=D:\\\\wood-1.0\repository")) {
+			properties.load(reader);
+		}
+		assertThat(properties.get("repository.dir"), equalTo("D:\\wood-1.0"));
 	}
 }
