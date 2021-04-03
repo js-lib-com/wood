5f235e47f5c0d805e416df737a75be0d51eb6726
Iulian Rotaru <iulian@gnotis.ro>
2021-04-03 21:25:26 +0300

    Create skeleton implementation and test cases for CLI component update.

--- wood-cli/src/main/java/js/wood/cli/compo/CompoUpdate.java
+++ wood-cli/src/main/java/js/wood/cli/compo/CompoUpdate.java
@@ -1,13 +1,24 @@
 package js.wood.cli.compo;
 
+import static java.lang.String.format;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 import js.wood.cli.ExitCode;
 import js.wood.cli.Task;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Spec;
 
 @Command(name = "update", description = "Update component to latest compatible version from repository.")
 public class CompoUpdate extends Task {
+	@Spec
+	private CommandSpec commandSpec;
+
 	@Option(names = { "-u", "--upgrade" }, description = "Update even if version major is different.")
 	private boolean upgrade;
 
@@ -15,8 +26,31 @@ public class CompoUpdate extends Task {
 	private CompoName name;
 
 	@Override
-	protected ExitCode exec() throws Exception {
-		print("Update component %s", name);
+	protected ExitCode exec() throws IOException {
+		if (!name.isValid()) {
+			throw new ParameterException(commandSpec.commandLine(), format("Component %s not found.", name.value()));
+		}
+
+		Path projectDir = files.getProjectDir();
+		Path compoDir = projectDir.resolve(name.path());
+		if (!files.exists(compoDir)) {
+			console.print("Missing component directory %s.", compoDir);
+			console.print("Command abort.");
+			return ExitCode.ABORT;
+		}
+
+		console.print("Update component %s", name);
 		return ExitCode.SUCCESS;
+	}
+
+	// --------------------------------------------------------------------------------------------
+	// Tests support
+
+	void setCommandSpec(CommandSpec commandSpec) {
+		this.commandSpec = commandSpec;
+	}
+
+	void setName(CompoName name) {
+		this.name = name;
 	}
 }
