1517def2984823fb3592a0c20f09b4bb877c24b2
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2022-10-17 21:07:11 +0300

    Rename CSS variables and style reset files.

--- wood-core/src/main/java/com/jslib/wood/ThemeStyles.java
+++ wood-core/src/main/java/com/jslib/wood/ThemeStyles.java
@@ -4,25 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ThemeStyles {
-	private final FilePath reset;
-	private final FilePath fx;
+	private final FilePath variables;
+	private final FilePath defaultStyles;
+	private final FilePath animations;
 	private final List<FilePath> styles = new ArrayList<>();
 
 	public ThemeStyles(FilePath themeDir) {
-		FilePath reset = null;
-		FilePath fx = null;
+		FilePath variables = null;
+		FilePath defaultStyles = null;
+		FilePath animations = null;
 		
 		for (FilePath file : themeDir) {
 			if(!file.isStyle()) {
 				continue;
 			}
 			switch (file.getName()) {
-			case CT.RESET_CSS:
-				reset = file;
+			case CT.VARIABLES_CSS:
+				variables = file;
+				break;
+				
+			case CT.DEFAULT_CSS:
+				defaultStyles = file;
 				break;
 
 			case CT.FX_CSS:
-				fx = file;
+				animations = file;
 				break;
 
 			default:
@@ -32,16 +38,21 @@ public class ThemeStyles {
 			}
 		}
 		
-		this.reset = reset;
-		this.fx = fx;
+		this.variables = variables;
+		this.defaultStyles = defaultStyles;
+		this.animations = animations;
 	}
 
-	public FilePath getReset() {
-		return reset;
+	public FilePath getVariables() {
+		return variables;
 	}
 
-	public FilePath getFx() {
-		return fx;
+	public FilePath getDefaultStyles() {
+		return defaultStyles;
+	}
+
+	public FilePath getAnimations() {
+		return animations;
 	}
 
 	public List<FilePath> getStyles() {
